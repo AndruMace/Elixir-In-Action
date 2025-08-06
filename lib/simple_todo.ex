0890cc660ad7b3663def1832c19002b23ad4d9f8
AndruMace <amacedeveloper@gmail.com>
2025-08-05 21:43:14 -0500

    moving to desktop

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -92,3 +92,16 @@ defimpl String.Chars, for: TodoList do
     "Todo"
   end
 end
+
+defimpl Collectable, for: TodoList do
+  def into(og) do
+    {og, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end
