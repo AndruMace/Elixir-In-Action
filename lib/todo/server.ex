6ffac214bed76d058164a014c6296d74eb1f4985
AndruMace <amacedeveloper@gmail.com>
2025-08-14 23:06:16 -0500

    moving to desktop, refactored modules

---
+++ lib/todo/server.ex
@@ -0,0 +1,34 @@
+defmodule Todo.Server do
+  use GenServer
+
+  # client
+
+  def start do
+    GenServer.start(Todo.Server, nil)
+  end
+
+  def add_entry(pid, new_entry) do
+    GenServer.cast(pid, {:add_entry, new_entry})
+  end
+
+  def entries(pid, date) do
+    GenServer.call(pid, {:entries, date})
+  end
+
+  # server
+
+  @impl GenServer
+  def init(_) do
+    {:ok, Todo.List.new()}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, Todo.List.entries(todo_list, date), todo_list}
+  end
+end
