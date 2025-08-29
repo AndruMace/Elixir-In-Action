719595d39132a6ea0ff504f95c2f0fe82848376d
AndruMace <amacedeveloper@gmail.com>
2025-08-28 21:20:36 -0500

    converted database.ex away from genservers

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -2,7 +2,7 @@ defmodule Todo.Database do
   @pool_size 3
   @db_folder "./persist"
 
-  def start_link(_) do
+  def start_link do
     File.mkdir_p!(@db_folder)
 
     children = Enum.map(1..@pool_size, &worker_spec/1)
@@ -26,30 +26,15 @@ defmodule Todo.Database do
     |> Todo.DatabaseWorker.get(key)
   end
 
-  # Choosing a worker makes a request to the database server process. There we
-  # keep the knowledge about our workers, and return the pid of the corresponding
-  # worker. Once this is done, the caller process will talk to the worker directly.
+  def child_spec(_) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
+  end
+
   defp choose_worker(key) do
-    GenServer.call(__MODULE__, {:choose_worker, key})
-  end
-
-  @impl GenServer
-  def init(_) do
-    IO.puts("Starting database server...")
-    File.mkdir_p!(@db_folder)
-    {:ok, start_workers()}
-  end
-
-  @impl GenServer
-  def handle_call({:choose_worker, key}, _, workers) do
-    worker_key = :erlang.phash2(key, 3)
-    {:reply, Map.get(workers, worker_key), workers}
-  end
-
-  defp start_workers() do
-    for index <- 1..3, into: %{} do
-      {:ok, pid} = Todo.DatabaseWorker.start_link(@db_folder)
-      {index - 1, pid}
-    end
+    :erlang.phash2(key, @pool_size) + 1
   end
 end
