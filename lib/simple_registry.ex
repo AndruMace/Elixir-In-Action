605258a9ba344600b9cb82c6a5ffa91290afab84
AndruMace <amacedeveloper@gmail.com>
2025-09-11 16:39:59 -0500

    completed ets process registry

--- lib/simple_registry.ex
+++ lib/simple_registry.ex
@@ -4,46 +4,37 @@ defmodule SimpleRegistry do
   def start_link, do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
   def register(name) do
-    GenServer.call(__MODULE__, {:register, name})
+    Process.link(Process.whereis(__MODULE__))
+
+    case :ets.insert_new(__MODULE__, {name, self()}) do
+      true -> :ok
+      false -> :error
+    end
   end
 
   def whereis(name) do
-    GenServer.call(__MODULE__, {:whereis, name})
+    case :ets.lookup(__MODULE__, name) do
+      [{^name, pid}] -> pid
+      [] -> nil
+    end
   end
 
   @impl GenServer
   def handle_info({:EXIT, pid, reason}, state) do
     IO.puts("Deleting #{pid} for #{reason}")
+    :ets.match_delete(__MODULE__, {:_, pid})
 
     {
       :noreply,
-      Map.reject(state, fn {_k, v} -> v == pid end)
+      state
     }
   end
 
   @impl GenServer
   def init(_) do
     IO.puts("Starting Simple Registry...")
-    :ets.new(__MODULE__, [:named_table, :public, write_concurrency: true])
     Process.flag(:trap_exit, true)
-    # {:ok, %{}}
-  end
-
-  @impl GenServer
-  def handle_call({:register, name}, _, state) do
-    {
-      :reply,
-      :ok,
-      Map.put(state, name, self())
-    }
-  end
-
-  @impl GenServer
-  def handle_call({:whereis, name}, _, state) do
-    {
-      :reply,
-      Map.get(state, name),
-      state
-    }
+    :ets.new(__MODULE__, [:named_table, :public, write_concurrency: true])
+    {:ok, nil}
   end
 end
